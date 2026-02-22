e3515d17a3304d7e24deb9346d37466844ca3165
Saish-Mhaskar <saishdmhaskar@gmail.com>
2026-02-22 14:10:33 +0530

    no change

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/public/index.html");
+})
+
+
 //Socket.io
 io.on('connection', (socket) => {
     socket.on("user-msg", (message) => {
         io.emit("msgSend",message);
     });
-})
-
-app.use(express.static(__dirname + "/public"));
-    
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/public/index.html");
 })
 
 server.listen(port, () => {
